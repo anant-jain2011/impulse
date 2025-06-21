c4f2b8df8baa87f03de5b7c06ff4bccca0cae4c2
Anant Jain <110173387+anant-jain2011@users.noreply.github.com>
2025-06-21 18:55:01 +0530

    Add files via upload

---
+++ chat/script.js
@@ -0,0 +1,252 @@
+const msg = document.getElementById("message");
+const msgBox = document.getElementById("messages");
+const om = document.getElementById("om");
+
+let chats = {
+    sent: [],
+    received: [],
+};
+
+const baseUrl = "https://vchat-4ldl.onrender.com";
+
+const eventSource = new EventSource(baseUrl + '/events');
+
+// Reconnection logic for EventSource
+eventSource.onerror = function (event) {
+    if (event.readyState === EventSource.CLOSED) {
+        // Reconnection logic here
+        console.log("Reconnecting to EventSource...");
+        eventSource.close();
+        eventSource = new EventSource(baseUrl + '/events');
+        console.log("error solved");
+    }
+};
+
+function decodeMessage(message, dateStr) {
+    const date = new Date(dateStr);
+    console.log("decoding message", message, date);
+
+    if (date.getFullYear() < 2024) {
+        return atob(atob(atob(message)));
+    } else {
+        return decodeURI(decodeURI(atob(message)));
+    }
+}
+
+async function getChats() {
+    const res = await fetch(baseUrl + "/getChats")
+
+    return res.json();
+}
+
+const displayMessage = (content, position, id, sender) => {
+    var urlRegex = /https?:\/\/[^\s/$.?#].[^\s]*/g;
+    if (content.match(urlRegex)) {
+        content = content.replace(content.match(urlRegex), `<a href="${content.match(urlRegex)}" target="_blank">${content.match(urlRegex)}</a>`);
+    }
+
+    const newMsg = document.createElement("span");
+    newMsg.classList.add(`msg`);
+    newMsg.classList.add(`msg-${position}`);
+    
+    if (id) {
+        newMsg.id = id;
+    }
+    
+    if (sender) {
+        const uname = document.createElement("span");
+        uname.innerHTML = sender ? `<b>${sender}</b> ` : "";
+        uname.classList.add("uname");
+        newMsg.appendChild(uname);
+    }
+    
+    newMsg.appendChild(document.createTextNode(content));
+    msgBox.appendChild(newMsg);
+};
+
+const deleteSelected = async () => {
+    const selected = localStorage.getItem("selected").trim().split(" ");
+
+    let res = await fetch(baseUrl + "/deletechats", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            selected,
+        }),
+    });
+
+    let data = await res.json();
+
+    if (data.success) {
+        selected.forEach(id => {
+            document.getElementById(id).remove();
+        });
+
+        localStorage.setItem("selected", "");
+
+        dlen.parentElement.style.display = "none";
+    }
+};
+
+window.onload = async () => {
+    if (localStorage.getItem("details") == null) {
+        location.href = "/";
+    }
+
+    let details = JSON.parse(localStorage.getItem("details"));
+
+    let { username, roomNumber, roomName } = details;
+
+    document.title = roomName;
+
+    roomname.innerHTML = roomName;
+    roomnumber.innerHTML = "Room Id - " + roomNumber;
+
+    msg.focus();
+
+    await getChats().then((messages) => {
+        messages.map((message) => {
+            if (message.roomNumber == roomNumber) {
+                if (message.sentFrom != username) {
+                    displayMessage(decodeMessage(message.content, message.createdAt), "left", message._id, message.sentFrom);
+                }
+
+                else {
+                    displayMessage(decodeMessage(message.content, message.createdAt), "right", message._id);
+                }
+
+                msgBox.parentElement.scrollTop = msgBox.parentElement.scrollHeight;
+            }
+        });
+    });
+
+    eventSource.addEventListener("message", (event) => {
+        let item = (typeof event.data) == "string" ? JSON.parse(event.data).newMessage : event.data.newMessage;
+
+        if (item.sentFrom != username && item.roomNumber == roomNumber) {
+            displayMessage(decodeMessage(item.content, item.createdAt), "left", item._id, item.sentFrom);
+
+            msgBox.parentElement.scrollTop = msgBox.parentElement.scrollHeight;
+        }
+    });
+
+    localStorage.setItem("selected", "");
+
+    const toggleSelected = (e) => {
+        let msg3 = e.target;
+        if (!msg3.classList.value.includes("msg")) {
+            msg3 = msg3.parentElement;
+        }
+
+        let prevBg = msg3.classList.value.includes("msg-left") ? "#fff" : "#25D366";
+
+        if (localStorage.getItem("selected").includes(msg3.id)) {
+            localStorage.setItem("selected", localStorage.getItem("selected").replaceAll(msg3.id + " ", ""));
+            msg3.style.backgroundColor = prevBg;
+
+            console.log("already selected, deselecting it", msg3, localStorage.getItem("selected"));
+        }
+
+        else {
+            localStorage.setItem("selected", localStorage.getItem("selected") + msg3.id + " ");
+            msg3.style.backgroundColor = "gray";
+
+            console.log("unselected, selecting it", msg3, localStorage.getItem("selected"));
+        }
+
+        dlen.innerHTML = "Delete " + localStorage.getItem("selected").trim().split(" ").length + " Chat(s)";
+
+        if (localStorage.getItem("selected").trim() == "") {
+            dlen.parentElement.style.display = "none";
+            document.querySelectorAll(".msg").forEach(msg2 => msg2.onclick = () => null);
+
+            localStorage.setItem("selectionMode", false);
+            console.log("all cleared");
+        }
+    };
+
+    document.querySelectorAll(".msg").forEach(msg => {
+        msg.addEventListener("mousedown", () => {
+            let timeOut = setTimeout(() => {
+                msg.style.backgroundColor = "gray";
+                dlen.parentElement.style.display = "flex";
+
+                if (!localStorage.getItem("selected").includes(msg.id)) {
+                    localStorage.setItem("selected", localStorage.getItem("selected") + msg.id + " ");
+                }
+
+                dlen.innerHTML = "Delete " + localStorage.getItem("selected").trim().split(" ").length + " Chat(s)";
+
+                localStorage.setItem("selectionMode", true);
+            }, 1000);
+
+            localStorage.setItem("timeOut", timeOut);
+        });
+
+        msg.addEventListener("mouseup", () => {
+            clearTimeout(localStorage.getItem("timeOut"));
+
+            if (eval(localStorage.getItem("selectionMode"))) {
+                document.querySelectorAll(".msg").forEach(msg2 => msg2.onclick = toggleSelected);
+            }
+        });
+    });
+
+    openOptionsMenu();
+};
+
+const sendMessage = () => {
+    if (msg.value != "") {
+        chats.sent.push(msg.value);
+        localStorage.setItem("chats", JSON.stringify(chats));
+
+        displayMessage(msg.value, "right");
+        msgBox.parentElement.scrollTop = msgBox.parentElement.scrollHeight;
+
+        let details = JSON.parse(localStorage.getItem("details"));
+
+        let { username, roomNumber } = details;
+
+        (async function () {
+            const res = await fetch(baseUrl + "/chat", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userName: username,
+                    roomNumber: roomNumber,
+                    content: msg.value
+                }),
+            });
+        })();
+
+        msg.value = "";
+    }
+    else {
+        alert("Can't send empty message!");
+    }
+};
+
+const openOptionsMenu = () => {
+    let cond = om.style.display == "none";
+    om.style.display = cond ? "flex" : "none";
+    // document.getElementsByClassName("m-bar")[0].classList.toggle("m-bar2");
+};
+
+const clearChats = () => {
+    const doClear = confirm("Do you sure want to clear your chats?");
+    if (doClear) {
+        chats.sent = [];
+        localStorage.setItem("chats", JSON.stringify(chats));
+        msgBox.innerHTML = "";
+    }
+};
+
+function leaveRoom() {
+    localStorage.removeItem("details");
+
+    location.href = "/";
+}
